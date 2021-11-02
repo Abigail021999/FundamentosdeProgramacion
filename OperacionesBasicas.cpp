1fc82c5ed5c2c8c3b1b5f8c77503d6a5e934a32e
Abigail021999 <92316895+Abigail021999@users.noreply.github.com>
2021-11-01 19:30:49 -0500

    Proyecto 1

---
+++ OperacionesBasicas.cpp
@@ -0,0 +1,44 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	//para la suma;
+	float x1,x2,s;
+	//para la resta;
+	float x3,x4,r;
+	//para la division;
+	float x5,x6,d;
+	//para la multiplicacion;
+	float x7,x8,m;
+
+	cout<<"Ingrese los valores a sumar : ";
+	cout<<"Ingrese x1 : ";cin>>x1;
+	cout<<"Ingrese x2 : ";cin>>x2;
+
+	cout<<"Ingrese los valores a sumar : ";
+	cout<<"Ingrese x1 : ";cin>>x1;
+	cout<<"Ingrese x2 : ";cin>>x2;
+
+	cout<<"Ingrese los valores a sumar : ";
+	cout<<"Ingrese x1 : ";cin>>x1;
+	cout<<"Ingrese x2 : ";cin>>x2;
+
+	cout<<"Ingrese los valores a sumar : ";
+	cout<<"Ingrese x1 : ";cin>>x1;
+	cout<<"Ingrese x2 : ";cin>>x2;
+
+	s=x1+x2;
+	r=x3-x4;
+	m=x5*x6;
+	d=x7/x8;
+
+	cout<<"El resultado de la suma fue: "<<s<<endl;
+	cout<<"El resultado de la resta fue: "<<r<<endl;
+	cout<<"El resultado de la multiplicacion fue: "<<m<<endl;
+	cout<<"El resultado de la division fue: "<<d<<endl;
+
+	return(0);
+}
+
+
